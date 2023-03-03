5f92fea160c31bf958653ec803b1d63843c59dc8
dgrammatiko <d.grammatiko@gmail.com>
2023-03-03 15:55:06 +0100

    updates

--- site/_data/assets.js
+++ site/_data/assets.js
@@ -1,7 +1,7 @@
 const { readFile } = require('fs').promises;
 const rollup = require('rollup');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const { terser } = require("rollup-plugin-terser");
+const terser = require("@rollup/plugin-terser");
 const template = require("rollup-plugin-html-literals");
 const { existsSync, mkdirSync } = require('fs');
 const postcss  = require('postcss')
